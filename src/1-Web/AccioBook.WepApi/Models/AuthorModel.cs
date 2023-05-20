47e2879b55a011ca7928d47152012093f2ad0117
Caroline-eng <115654361+Caroline-eng@users.noreply.github.com>
2023-05-20 20:42:11 -0300

    [feat] - novos métodos userController e ignorar id no swagger

--- src/1-Web/AccioBook.WepApi/Models/AuthorModel.cs
+++ src/1-Web/AccioBook.WepApi/Models/AuthorModel.cs
@@ -1,9 +1,11 @@
 ﻿using AccioBook.Domain.Entities;
+using System.Text.Json.Serialization;
 
 namespace AccioBook.WepApi.Models
 {
     public class AuthorModel
     {
+        [JsonIgnore]
         public Int64 Id { get; set; }
         public string Name { get; set; }
     }
--- src/1-Web/AccioBook.WepApi/Models/GenreSearchModel.cs
+++ src/1-Web/AccioBook.WepApi/Models/GenreSearchModel.cs
@@ -1,9 +1,11 @@
 ﻿using AccioBook.Domain.Entities;
+using System.Text.Json.Serialization;
 
 namespace AccioBook.WepApi.Models
 {
     public class GenreSearchModel
     {
+        [JsonIgnore]
         public Int64 Id { get; set; }
         public Int64 Id_User { get; set; }
         public User User { get; set; }
